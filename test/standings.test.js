64fd79577a8af5eb61e5ec9fe009372220cdcb66
Hygmod <100744682+Hygmod@users.noreply.github.com>
2026-05-15 17:21:23 -0700

    Test standings publish threshold

--- test/standings.test.js
+++ test/standings.test.js
@@ -20,6 +20,7 @@ test("sorts riders by points and marks missing volunteer days", () => {
   assert.equal(category.riders[0].provisional, false);
   assert.equal(category.riders[1].displayName, "Myles Villahermosa");
   assert.equal(category.riders[1].provisional, true);
+  assert.equal(category.riders[2].displayName, "John Zaine");
 });
 
 test("publishes kids by first name only", () => {
@@ -36,4 +37,26 @@ test("publishes kids by first name only", () => {
   const kids = standings.categories.find((item) => item.sheetName === "Kids");
   assert.equal(kids.riders[0].displayName, "Sam");
   assert.equal(kids.riders[0].raceNumber, "12");
+});
+
+test("does not publish roster-only or volunteer-only categories", () => {
+  const standings = buildStandings({
+    "B's": [
+      ["Chico Airport Crit Standings"],
+      ["CAT B"],
+      [""],
+      ["Last Name", "First Name", "Racer #", "Total", "Vol", "5/5", "5/12"],
+      ["Starmer", "Kellen", "80", "0", "1", "v", ""],
+      ["Rieniets", "Travis", "34", "0", "1", "", "V"]
+    ],
+    Kids: [
+      ["Chico Airport Crit Standings"],
+      ["Kids"],
+      [""],
+      ["Last Name", "First Name", "Racer #", "Total", "Vol", "5/5"],
+      ["Example", "Sam", "12", "0", "0", ""]
+    ]
+  }, "2026-05-15T00:00:00.000Z");
+
+  assert.deepEqual(standings.categories, []);
 });
